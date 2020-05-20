da78078ca72abb0fab5c3f89f2aad62e7c77744e
rcorojr <61759545+rcorojr@users.noreply.github.com>
2020-05-20 11:22:20 -0600

    link js. add bootstrap link. add firebase link/config. add moment link.

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBZfmZnbyx2oHce0WHTIorvU7SPvgY2H4k",
+    authDomain: "timesheet-2f5be.firebaseapp.com",
+    databaseURL: "https://timesheet-2f5be.firebaseio.com",
+    projectId: "timesheet-2f5be",
+    storageBucket: "timesheet-2f5be.appspot.com",
+    messagingSenderId: "252647608411",
+    appId: "1:252647608411:web:c48a5534aa62aebe39ef7e"
+  };
+
+  firebase.initializeApp(firebaseConfig);
